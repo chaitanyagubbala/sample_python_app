fff2ba3f28b1226d314d952e507deac0bf28675e
chaitanyagubbala <41671692+chaitanyagubbala@users.noreply.github.com>
2022-04-06 22:26:23 +0530

    created main.py file and added sample python code

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+list1 = [8,9,3,5,1,6,2]
+print("The reversed list is ".format(list1[::-1]))
+
+list2 = [28,39,46,76,93,6,72,12,54]
+print("The sorted list is ".format(list2.sort())
+      
+list3=[]
+list3 = list1.copy()
+print("This is the copy of list 1 ".format(list3))
